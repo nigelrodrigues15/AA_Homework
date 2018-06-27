0901965debc29391e9ea56908f9cca99ecaeaea3
Nigel Rodrigues <nigel1@ualberta.ca>
2018-06-27 07:43:46 -0700

    Homework completed

--- W2D3/spec/dessert_spec.rb
+++ W2D3/spec/dessert_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require 'dessert'
+require "chef"
 
 =begin
 Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
@@ -7,7 +8,7 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 
 describe Dessert do
   subject(:dessert) { Dessert.new("dessert", 2, "baker")}
-  let(:chef) { double("chef") }
+  let(:chef) { double(Chef.new("baker")) }
 
   describe "#initialize" do
     it "sets a type" do
@@ -23,30 +24,49 @@ describe Dessert do
     end
 
     it "raises an argument error when given a non-integer quantity" do
-      expect{Dessert.new("dessert", "two"", "baker")}.to raise_error(ArgumentError)
+      expect{Dessert.new("dessert", "two", "baker")}.to raise_error(ArgumentError)
     end
 
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient("chocolate")
+      expect(dessert.ingredients).to include("chocolate")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredient = ["chocolate", "sugar", "syrup"]
+      ingredient.each { |el| dessert.add_ingredient(el) }
+      dessert.mix!
+      expect(dessert.ingredients).not_to eq(ingredient)
+      expect(dessert.ingredients.sort).to eq(ingredient.sort)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      dessert.eat(1)
+      expect(dessert.quantity).to eq(1)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect{dessert.eat(3)}.to raise_error(RuntimeError)
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      expect(dessert.serve).to include("Baker")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    subject(:chef) { Chef.new("baker")}
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef.bake(dessert)).to eq(400)
+    end
   end
 end
